64fc78ea8f13792abe1ce08a761db4b6d2af52df
unknown <mdmuktarali100@gmail.com>
2017-10-20 17:47:32 +0600

    CSS Changes and some class add vapiano pages

--- src/welcome.vue
+++ src/welcome.vue
@@ -40,7 +40,7 @@
                     <ons-carousel-item v-for="foo in food['foods']"  style="background-color: #085078;">
                         <div class="image_footer">
                             <a href="" v-if="foo.img"><img v-bind:src="foo.img" /></a>
-                            <a href="" ><img :src="footercarousel" /></a>
+                            <a href="" v-else><img :src="footercarousel" /></a>
 
                             <!--<img :src="footercarousel" alt="" />-->
                             <div class="image_overlay_content">
@@ -121,14 +121,7 @@
     }
 </script>
 <style scoped>
-<<<<<<< HEAD
-.page--material__background{
-    background:red;
 
-}
-.welcome_bg{
-=======
->>>>>>> be4fe3ebfe89020a8083261861987745861557e7
 
 
 
